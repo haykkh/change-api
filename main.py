4382b51d96a220c213379ebcce5b95f2f33139fc
Hayk Khachatryan <hi@hayk.io>
2020-11-09 22:00:00 +0400

    hello world

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+
 import json
+
 
 app = FastAPI()
 
@@ -11,4 +13,12 @@ def read_root():
 
 @app.get("/data")
 def read_data():
-    return json.load(open('/home/ec2-user/data.json'))
+    data = json.load(open('/home/ec2-user/data.json'))
+    res =  f"""<?xml version="1.0"?>
+    <data>
+        <signatures>{data['signatures']}</signatures>
+        <views>{data['views']}</views>
+        <shares>{data['shares']}</shares>
+    </data>
+    """
+    return Response(content=res, media_type="application/xml")
